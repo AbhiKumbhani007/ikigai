4ccf0fbaa832cb03ff1f42960e3878a6ec3c3982
AbhiKumbhani007 <abhi1234kumbhani@gmail.com>
2022-04-06 20:04:35 +0530

    In Our ParterPage New Hardcoded partner Data added and given website links alos provided

--- lib/screens/partner_screen/partner_screen.dart
+++ lib/screens/partner_screen/partner_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PartnerScreen extends StatefulWidget {
   const PartnerScreen({Key? key}) : super(key: key);
@@ -35,14 +36,14 @@ class _PartnerScreenState extends State<PartnerScreen> {
                   leading: CircleAvatar(
                     radius: 30,
                     backgroundImage: const Image(
-                            image: AssetImage('assets/images/google_logo.png'))
+                            image: AssetImage('assets/images/sarthhy.jpg'))
                         .image,
                     backgroundColor: Colors.deepPurple,
                   ),
-                  title: Text('Google',
+                  title: Text('Sarrthy ',
                       style: GoogleFonts.prompt(
                           fontSize: 24, color: Colors.deepPurple)),
-                  subtitle: Text('Google LLC',
+                  subtitle: Text('Your neighbour hood delivery partner',
                       style: GoogleFonts.prompt(fontSize: 16)),
                 ),
               ),
@@ -64,17 +65,89 @@ class _PartnerScreenState extends State<PartnerScreen> {
               elevation: 2,
               child: Center(
                 child: ListTile(
+                  onTap: _launchInstaJobOfferURL,
                   leading: CircleAvatar(
                     radius: 30,
                     backgroundImage: const Image(
-                      image: AssetImage('assets/images/insta_logo.png'),
+                      image:
+                          AssetImage('assets/images/partner_base_logo_1.jpg'),
                     ).image,
                     backgroundColor: Colors.deepPurple,
                   ),
-                  title: Text('Instagram',
+                  title: Text('instajoboffer',
                       style: GoogleFonts.prompt(
                           fontSize: 24, color: Colors.deepPurple)),
-                  subtitle: Text('Instagram LLC',
+                  subtitle: Text('Fastest way to get your dream job',
+                      style: GoogleFonts.prompt(
+                          fontSize: 16, color: Colors.black)),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.02,
+              vertical: MediaQuery.of(context).size.height * 0.01),
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              color: Color.fromARGB(255, 255, 255, 255),
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.white70, width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 2,
+              child: Center(
+                child: ListTile(
+                  onTap: _launchYouthForSevarURL,
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: const Image(
+                      image:
+                          AssetImage('assets/images/partner_base_logo_1.jpg'),
+                    ).image,
+                    backgroundColor: Colors.deepPurple,
+                  ),
+                  title: Text('Youth for Seva',
+                      style: GoogleFonts.prompt(
+                          fontSize: 24, color: Colors.deepPurple)),
+                  subtitle: Text('', // Add Description Here
+                      style: GoogleFonts.prompt(
+                          fontSize: 16, color: Colors.black)),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.02,
+              vertical: MediaQuery.of(context).size.height * 0.01),
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              color: Color.fromARGB(255, 255, 255, 255),
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.white70, width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 2,
+              child: Center(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: Image(
+                      image: AssetImage(
+                        'assets/images/gosave.jpg',
+                      ),
+                    ).image,
+                    backgroundColor: Colors.deepPurple,
+                  ),
+                  title: Text('Go save',
+                      style: GoogleFonts.prompt(
+                          fontSize: 24, color: Colors.deepPurple)),
+                  subtitle: Text('', // Add Description Here
                       style: GoogleFonts.prompt(
                           fontSize: 16, color: Colors.black)),
                 ),
@@ -100,14 +173,15 @@ class _PartnerScreenState extends State<PartnerScreen> {
                   leading: CircleAvatar(
                     radius: 30,
                     backgroundImage: const Image(
-                      image: AssetImage('assets/images/demo_logo.png'),
+                      image:
+                          AssetImage('assets/images/partner_base_logo_1.jpg'),
                     ).image,
                     backgroundColor: Colors.deepPurple,
                   ),
-                  title: Text('Brijesh Patadiya',
+                  title: Text('C A Shekhar',
                       style: GoogleFonts.prompt(
                           fontSize: 24, color: Colors.deepPurple)),
-                  subtitle: Text('Flutter Devloper',
+                  subtitle: Text('Financial advisory services',
                       style: GoogleFonts.prompt(
                           fontSize: 16, color: Colors.black)),
                 ),
@@ -117,5 +191,23 @@ class _PartnerScreenState extends State<PartnerScreen> {
         ),
       ],
     );
+  }
+}
+
+_launchInstaJobOfferURL() async {
+  const url = 'http://instajoboffer.com/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchYouthForSevarURL() async {
+  const url = 'https://www.youthforseva.org/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
